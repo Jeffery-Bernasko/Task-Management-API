1c3e073112b1f2b18f7b0e594af3a6869d741a35
Jeffery Bernasko <jefferypeter20@gmail.com>
2025-02-12 21:49:19 +0000

    implement the filtering by writing JPQL

--- src/main/java/org/example/taskmanagement/repository/TaskRepository.java
+++ src/main/java/org/example/taskmanagement/repository/TaskRepository.java
@@ -1,2 +1,21 @@
-package org.example.taskmanagement.repository;public interface TaskRepository {
+package org.example.taskmanagement.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.example.taskmanagement.enums.TaskPriority;
+import org.example.taskmanagement.enums.TaskStatus;
+import org.example.taskmanagement.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    @Query("SELECT t FROM Task t WHERE " +
+            "(:status IS NULL OR t.status = :status) AND " +
+            "(:priority IS NULL OR t.priority = :priority)")
+    List<Task> filterTask(@Param("status") TaskStatus status, @Param("priority") TaskPriority priority);
 }
